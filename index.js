7a98cc24a09ab74c64530e21c77d21624c308a47
xnor <titus.um.78@gmail.com>
2025-05-24 15:51:46 +0200

    aze

---
+++ index.js
@@ -0,0 +1,11 @@
+[
+  {
+    "id": "hentaiorigines",
+    "name": "Hentai Origines",
+    "file": "hentaiorigines.aix",
+    "icon": "hentaiorigines.png",
+    "lang": "fr",
+    "version": 7,
+    "nsfw": 0
+  }
+]
